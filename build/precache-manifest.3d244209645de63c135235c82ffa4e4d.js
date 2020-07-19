bc497e6bc09fc88cfaf98540bc850a23df3278a3
josue <josretamozovar@gmail.com>
2020-07-19 10:31:13 -0500

    added build

---
+++ build/precache-manifest.3d244209645de63c135235c82ffa4e4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16282d94bd8ec70090252cc4776d6b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "358591cd5f36d84d3ef9",
+    "url": "/static/css/main.8f8a0b02.chunk.css"
+  },
+  {
+    "revision": "15072f9d2228d7584c75",
+    "url": "/static/js/2.959befaf.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.959befaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "358591cd5f36d84d3ef9",
+    "url": "/static/js/main.cae9a8b7.chunk.js"
+  },
+  {
+    "revision": "6650a52160968d3c990f",
+    "url": "/static/js/runtime-main.8888d775.js"
+  }
+]);
